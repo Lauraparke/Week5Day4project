c572ac75d3b24109958bd0c68a0b2fb7fa742881
Lauraparke <75710627+Lauraparke@users.noreply.github.com>
2021-01-30 17:53:56 -0700

    5 commits

---
+++ app.js
@@ -0,0 +1,14 @@
+var navItems = $('.navItem');
+$('.navItem').css('color', 'white');
+
+var navs = $('#nav');
+$('#nav').css('background-color', '#145');
+
+var lists = $('li:contains("even")');
+$('li:contains("even")').css('background-color', '#ccc');
+
+var lists = $('li:contains("odd")');
+$('li:contains("odd")').css('background-color', '#aaa');
+
+var inputs = $(':input[type="text"]');
+$(':input[type="text"]').val('Laura');
